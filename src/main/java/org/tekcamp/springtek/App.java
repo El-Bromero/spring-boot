2380501a71c94ae27828df669c1a2a2c0d0ad094
El-Bromero <brandonaromero@gmail.com>
2021-04-12 17:42:45 -0500

    Finished beanstalk section. Did convoluted way of pairing tms with students and printing it since I had to conform to what was set up for the function beforehand

--- src/main/java/org/tekcamp/springtek/App.java
+++ src/main/java/org/tekcamp/springtek/App.java
@@ -1,5 +1,6 @@
 package org.tekcamp.springtek;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,6 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.tekcamp.springtek.beans.beanstalk.Beanstalk;
+import org.tekcamp.springtek.beans.beanstalk.Jack;
+import org.tekcamp.springtek.beans.calc.Calculator;
 
 import java.util.Arrays;
 
@@ -19,6 +23,15 @@ public class App {
 
 	@Value("${spring.application.name}")
 	private String appName;
+
+	@Autowired
+	Calculator calc;
+
+	@Autowired
+	Beanstalk beanstalk;
+
+	@Autowired
+	Jack jack;
 
 	public static void main(String[] args) {
 		ApplicationContext container = SpringApplication.run(App.class, args);
@@ -33,6 +46,20 @@ public class App {
 		return args -> {
 			System.out.println(appName + " running on port " + portNum);
 			System.out.println("navigate to http://localhost:" + portNum + "/greet for the HelloWorld Page");
+
+			int x = 10;
+			int y = 5;
+			System.out.println("Calculator:");
+			System.out.println(x + " + " + y + " = " + calc.add(x,y));
+			System.out.println(x + " - " + y + " = " + calc.subtract(x,y));
+			System.out.println(x + " * " + y + " = " + calc.multiply(x,y));
+			System.out.println(x + " / " + y + " = " + calc.divide(x,y));
+
+			System.out.println("Beanstalk:");
+			jack.showPeople();
+			for (String i : jack.pairTmWithStudents().keySet()) {
+				System.out.println(i + ": " + Arrays.toString(jack.pairTmWithStudents().get(i)));
+			}
 		};
 	}
 
--- src/main/java/org/tekcamp/springtek/beans/beanstalk/Beanstalk.java
+++ src/main/java/org/tekcamp/springtek/beans/beanstalk/Beanstalk.java
@@ -1,8 +1,22 @@
 package org.tekcamp.springtek.beans.beanstalk;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Beanstalk {
 	//this class should produce beans to inject into the Spring Container
 	//create two Beans that return the arrays stored on this class, that will be injected into the Spring Container
 	private String[] tms = new String[]{"Alex","Craig","Desaree","Marcelo","Matt"};
 	private String[] students = new String[]{"Anna","Alex","Brandon","Brittany","Cameron","Cason","Chad","Chris Santiago","Chris Jones","Ed","Rinn","Irina","Jacob","Jaleel","James","Jorge","Jude","Kyle","Mariana","Monica","Paul","Ryan","Shannon","Sikeisha","Taylor"};
+
+	@Bean
+	String[] getTms() {
+		return this.tms;
+	}
+
+	@Bean
+	String[] getStudents() {
+		return this.students;
+	}
 }
